138a6d4e2e811c5dee577e7fbd7e8c929334af4f
glowredman <35727266+glowredman@users.noreply.github.com>
2024-01-24 21:00:38 +0100

    Add mod stub

---
+++ src/main/java/glowredman/biomebridge/BiomeBridge.java
@@ -0,0 +1,14 @@
+package glowredman.biomebridge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(acceptedMinecraftVersions = "1.7.10", modid = BiomeBridge.MODID, name = "BiomeBridge", version = Tags.VERSION)
+public class BiomeBridge {
+    
+    public static final String MODID = "biomebridge";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+}
